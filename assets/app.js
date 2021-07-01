d6927722abea5108b451569e11f9d8d49c11c763
Thiago Silva Lopes <thiagodrive08@hotmail.com>
2021-07-01 15:35:30 -0300

    Menu existe mas ainda não está funcional

--- assets/app.js
+++ assets/app.js
@@ -57,3 +57,23 @@ darkModeBtn.addEventListener("click", () => {
   darkMode.toggleAttribute("darkTheme");
   darkModeBtn.toggleAttribute("active");
 });
+
+/* Menu hambúrguer: */
+
+const btnMobile = document.getElementById("btn-mobile");
+
+function toggleMenu(event) {
+  if (event.type === "touchstart") event.preventDefault();
+  const nav = document.getElementById("nav");
+  nav.classList.toggle("active");
+  const active = nav.classList.contains("active");
+  event.currentTarget.setAttribute("aria-expanded", active);
+  if (active) {
+    event.currentTarget.setAttribute("aria-label", "Fechar Menu");
+  } else {
+    event.currentTarget.setAttribute("aria-label", "Abrir Menu");
+  }
+}
+
+btnMobile.addEventListener("click", toggleMenu);
+btnMobile.addEventListener("touchstart", toggleMenu);
